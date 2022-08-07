c0196888e0d7f6e2b70fe4467876fdf4ebdbb460
Vitaly Borodulin <rainbowf@mail.ru>
2022-08-07 22:58:30 +0500

    Remove unnecessary imports

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,5 @@
-import phonenumbers
 from django.http import JsonResponse
 from django.templatetags.static import static
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.serializers import (
